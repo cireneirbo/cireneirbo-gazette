848a0f65278973ea7b05c2135cb90f75c4a69fd2
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-03-27 14:16:08 -0400

    updated fetchnews.js to call the api

--- ClientApp/src/components/FetchNews.js
+++ ClientApp/src/components/FetchNews.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import NewsAPI from 'newsapi';
 
 export class FetchNews extends Component {
     static displayName = FetchNews.name;
@@ -52,8 +53,81 @@ export class FetchNews extends Component {
     }
 
     async populateNewsData() {
-        const response = await fetch('newsarticles');
+        const newsapi = new NewsAPI('API_KEY');
+
+        //const response = await fetch('newsarticles');
+
+        const response = await newsapi.v2.topHeadlines({
+            sources: 'bbc-news,the-verge',
+            q: 'bitcoin',
+            category: 'business',
+            language: 'en',
+            country: 'us'
+        }).then(response => {
+            console.log(response);
+        });
+
         const data = await response.json();
         this.setState({ articles: data, loading: false });
     }
 }
+/*
+
+
+/////////////////////////////////
+
+
+//const NewsAPI = require('newsapi');
+//const newsapi = new NewsAPI('API_KEY');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+    sources: 'bbc-news,the-verge',
+    q: 'bitcoin',
+    category: 'business',
+    language: 'en',
+    country: 'us'
+}).then(response => {
+    console.log(response);
+    /*
+      {
+        status: "ok",
+        articles: [...]
+      }
+    *//*
+});
+// To query /v2/everything
+// You must include at least one q, source, or domain
+newsapi.v2.everything({
+    q: 'bitcoin',
+    sources: 'bbc-news,the-verge',
+    domains: 'bbc.co.uk, techcrunch.com',
+    from: '2017-12-01',
+    to: '2017-12-12',
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 2
+}).then(response => {
+    console.log(response);
+    /*
+      {
+        status: "ok",
+        articles: [...]
+      }
+    *//*
+});
+// To query sources
+// All options are optional
+newsapi.v2.sources({
+    category: 'technology',
+    language: 'en',
+    country: 'us'
+}).then(response => {
+    console.log(response);
+    /*
+      {
+        status: "ok",
+        sources: [...]
+      }
+    *//*
+});*/
